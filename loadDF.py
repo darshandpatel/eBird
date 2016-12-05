3ba7d8e4c0893a979e1d496e9c121349c2a4210f
sriramprabhus <sankaraguru.s@husky.neu.edu>
2016-12-04 22:54:12 -0500

    replace ? with 0

--- loadDF.py
+++ loadDF.py
@@ -1,5 +1,10 @@
 import pyspark
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import column
+def customfunction(x):
+    if x == "?":
+        x = "0"
+    return x
 from pyspark.sql import SQLContext
 conf = (SparkConf()
          .setMaster("local")
@@ -9,5 +14,11 @@ sc = SparkContext(conf = conf)
 sqc = SQLContext(sc)
 sample = sc.textFile("./sample/part-00000").persist()
 sampledf = sample.map(lambda x : x.split(",")).toDF()
-newsampledf = sampledf.drop(sampledf._1)
-newsampledf.filter(sampledf._18 != "?").show(10)
+#to drop a column
+#newsampledf = sampledf.drop(sampledf._1)
+
+#to filter rows
+#newsampledf.filter(sampledf._18 != "?").show(10)
+
+#to replace ? with 0
+sampledf.replace("?","0").show(10)
