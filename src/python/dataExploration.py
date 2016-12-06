59077791d352ad04a82d8e59ad37d30cc2b50a97
Darshan Patel <patel.dars@husky.neu.edu>
2016-12-05 20:16:33 -0500

    Class is created to split the input data

--- src/python/dataExploration.py
+++ src/python/dataExploration.py
@@ -1,8 +1,24 @@
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("Machine Learning")
-sc = SparkContext(conf=conf)
 
-textFile = sc.textFile("/Users/Darshan/Documents/MapReduce/FinalProject/labeled.csv.bz2")
+class DataExploration:
 
-print textFile.first()
+    def __init__(self):
+        self.conf = SparkConf().setMaster("local").setAppName("Machine Learning")  # (SparkConf().setMaster("local").setAppName("eBird"))
+        self.sc = SparkContext(conf=self.conf)
+
+    def splitInputData(self, inputPath, outputPath):
+        seed = 17
+        labelled = self.sc.textFile(inputPath)
+        headers = labelled.first()
+        print headers
+        modifiedLabels = labelled.subtract(self.sc.parallelize(headers))
+        train, validate = modifiedLabels.randomSplit([9, 1], seed)
+        train1, sample = validate.randomSplit([9, 1], seed)
+        sample.saveAsTextFile(outputPath)
+
+if __name__ == "__main__":
+    inputPath = "/Users/Darshan/Documents/MapReduce/FinalProject/labeled.csv.bz2"
+    outputPath = "/Users/Darshan/Documents/MapReduce/FinalProject/LabeledSample"
+    dataExploration = DataExploration()
+    dataExploration.splitInputData(inputPath=inputPath, outputPath=outputPath)
